c1a50b302acc7fa01de21a266a86ebf4d548a045
gasimov <gasimov@toghrul>
2017-04-18 00:12:53 +0300

    Test2

--- Algorithms-eclipse/src/Main.java
+++ Algorithms-eclipse/src/Main.java
@@ -4,8 +4,9 @@ public class Main {
 
 	public static void main(String[] args) {
 		for(int i=0;i<10;i++){
-			System.out.println("11");
+			System.out.println("1111");
 		}
+		for(int i=0;i<10;i++);
 	}
 
 }
