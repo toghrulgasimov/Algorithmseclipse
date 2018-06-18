3b4bc781b55a09c4e7a28ac15521438848388c4e
root <root@gasimov>
2018-06-18 15:53:45 +0200

    Trie with Map

--- Algorithms-eclipse/src/strings/Trie.java
+++ Algorithms-eclipse/src/strings/Trie.java
@@ -9,37 +9,61 @@ package strings;
  *
  * @author Toghrul
  */
-public class Trie {
-    public Node root;
-    public class Node {
-        public Node parent;
-        public Node[] child;
-        public Node() {
-            child = new Node[26];
-            parent = null;
-        }
-    }
-    public Trie() {
-        root = new Node();
-    }
-    public void insert(String s) {
-        Node curr = root;
-        for(int i = 0; i < s.length(); i++) {
-            int a = s.charAt(i) - 'a';
-            if(curr.child[a] == null) {
-                curr.child[a] = new Node();
-                curr.child[a] = curr;
-            }
-            curr = curr.child[a];
-        }
-    }
-    public boolean search(String s) {
-        Node curr = root;
-        for(int i = 0; i < s.length(); i++) {
-            int a = s.charAt(i) - 'a';
-            if(curr.child[a] == null) return false;
-            curr = curr.child[a];
-        }
-        return true;
-    }
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+
+
+
+
+public  class Trie {
+	public  class Node {
+		public char c;
+		public int d;
+		public Map<Character, Node> M;
+		public Node(char c) {
+			this.c = c;
+			this.d = 1;
+			M = new HashMap<Character, Node>();
+		}
+	}
+	Node root;
+	public Trie() {
+		root = new Node(' ');
+	}
+	public void insert(String s) {
+		Node t = root;
+		for(int i = 0; i < s.length(); i++) {
+			Node k = t.M.get(s.charAt(i));
+			if(k == null) {
+				k = new Node(s.charAt(i));
+				t.M.put(s.charAt(i), k);
+				t = k;
+			}else {
+				
+				t = k;
+				t.d++;
+			}
+		}
+	}
+	public void delete(String s) {
+		Node t = root;
+		for(int i = 0; i < s.length(); i++) {
+			Node k = t.M.get(s.charAt(i));
+			if(k == null) {
+				return;
+			}else {
+				t = k;
+				t.d--;
+			}
+		}
+	}
+	public Node search(String s) {
+		Node t = root;
+		for(int i = 0; i < s.length(); i++) {
+			t = t.M.get(s.charAt(i));
+			if(t == null) return null;
+		}
+		return t;
+	}
 }
